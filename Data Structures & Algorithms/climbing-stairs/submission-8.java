71111df494726197e030de8e82f18be593289a1b
Ranjith K <166363170+Hawkayee@users.noreply.github.com>
2026-09-03 11:13:30 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        // Here we have the objective function of the problem 
+        // We have to find the distinct way of the climb the staris 
+        // We can go up 1 stairs or 2 stairs 
+        int dp[] = new int[n+1];
+        // Base cases
+        if(n < 2) return n;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-1] +dp[i-2];
+        }
+        return dp[n];
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+        
+
+}
