5c8bfa9d063b721e2a6e6215108f4007597aa213
Ranjith K <166363170+Hawkayee@users.noreply.github.com>
2026-09-08 22:01:47 +0530

    Add: minimum-path-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int dp[][] = new int[row][col];
+        // dp[0][0] = grid[0][0];
+
+        // for(int i =1; i < col; i++)
+        //   dp[0][i] = grid[0][i] + dp[0][i-1];
+
+        // for(int j =1; j < row; j++)
+        //   dp[j][0] = grid[j][0] + dp[j-1][0];
+        
+        return solve(row -1, col -1, dp, grid);
+    }
+
+    private int solve(int m, int n ,int dp[][], int arr[][]){
+        if(m == 0 && n == 0) return arr[0][0] ;
+        if(n < 0 || m < 0) return Integer.MAX_VALUE;
+        if(dp[m][n] != 0) return dp[m][n];
+        return dp[m][n] = Math.min(solve(m-1,n,dp,arr) , solve(m,n-1,dp,arr)) + arr[m][n];
+    }
+}
